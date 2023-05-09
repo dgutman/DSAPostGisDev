fe839d165372a8e849658ac6a62c4ead37565e90
David Gutman <dagutman@gmail.com>
2023-05-09 12:59:40 -0400

    cleaning

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -1,8 +1,9 @@
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, HTTPException
-from sqlmodel import Session
+from sqlmodel import Session, select
 from geoalchemy2.elements import WKTElement
 from geoalchemy2.functions import ST_Distance, ST_AsGeoJSON, ST_MakeEnvelope
+from sqlalchemy.orm import load_only
 from sqlalchemy import func, and_, delete
 import numpy as np
 import random, requests
@@ -10,7 +11,12 @@ import girder_client
 from typing import List
 
 from .services import engine, create_db_and_tables
-from .models import Cities, Stations, GasPrices, SimpleRectangles, DSAImage, tileFeatures
+from .models import (
+    SimpleRectangles,
+    DSAImage,
+    tileFeatures,
+    featureExtractionParams,
+)
 from .utils import extend_dict, pretify_address
 
 app = FastAPI()
@@ -28,31 +34,6 @@ def on_startup():
     create_db_and_tables()
 
 
-@app.post("/add-station/")
-async def add_station(station: Stations):
-    with Session(engine) as session:
-        exist = session.query(Stations).filter(Stations.station_id == station.station_id).first()
-        if exist:
-            raise HTTPException(status_code=400, detail="Station already exists")
-
-        point = f"POINT({station.longitude} {station.latitude})"
-        station.geom = WKTElement(point, srid=4326)
-
-        session.add(station)
-        session.commit()
-        session.refresh(station)
-
-        to_return = {}
-        to_return["station_id"] = station.station_id
-        to_return["latitude"] = station.latitude
-        to_return["longitude"] = station.longitude
-        to_return["cp"] = station.cp
-        to_return["city"] = station.city
-        to_return["adress"] = station.adress
-
-        return to_return
-
-
 @app.post("/addTileFeatures")
 async def post_tileFeatures(featureBatch: List[tileFeatures]):
     tileData = [tile_obj.dict() for tile_obj in featureBatch]
@@ -63,30 +44,115 @@ async def post_tileFeatures(featureBatch: List[tileFeatures]):
 
 
 @app.delete("/deleteTileFeatures")
-async def delete_tileFeatures():
+async def delete_tileFeatures(imageId: str, ftxtract_id: int):
     """This will delete tiles associated with an image in case I want to regenerate them"""
     with Session(engine) as session:
-        statement = delete(tileFeatures)
+        statement = (
+            delete(tileFeatures)
+            .where(tileFeatures.imageId == imageId)
+            .where(tileFeatures.ftxtract_id == ftxtract_id)
+        )
         result = session.exec(statement)
         session.commit()
         # print(result.rowcount)
         return "Tile Feature Data Truncated  "  # % result.rowCount
 
 
+## TO DO: Add optinoal parameter that will return all fields, not just the ones listed below
 @app.get("/getTileFeatures")
-async def get_tileFeatures():
-    print("Getting tile features")
+async def get_tileFeatures(imageId: str, ftxtract_id: int):
+    # print("Getting tile features based on internal imageId")
+
     with Session(engine) as session:
-        tileData = session.query(tileFeatures).limit(50).all()
+        tileData = (
+            session.query(tileFeatures)
+            .filter(tileFeatures.ftxtract_id == ftxtract_id)
+            .filter(tileFeatures.imageId == imageId)
+            .options(
+                load_only(
+                    tileFeatures.imageId,
+                    tileFeatures.topX,
+                    tileFeatures.topY,
+                    tileFeatures.width,
+                    tileFeatures.height,
+                    tileFeatures.average,
+                )
+            )
+            .limit(10000)
+            .all()
+        )
         return tileData
 
+    return None
+
+
+@app.get("/getFeatureSets")
+async def get_featureSets(
+    imageId: str,
+):
+    ### Get all the feature sets available for a given image
+    with Session(engine) as session:
+        featureSets = (
+            session.query(featureExtractionParams)
+            .filter(featureExtractionParams.imageId == imageId)
+            .all()
+        )
+        return featureSets
+    return None
+
+
+@app.post("/addFeatureExtractionParams")
+async def insert_featureExtractionParams(featXtractParams: featureExtractionParams):
+    print("Adding parameters used to do a certain tile feature extraction")
+    with Session(engine) as session:
+        exist = (
+            session.query(featureExtractionParams)
+            .filter(featureExtractionParams.imageId == featXtractParams.imageId)
+            .filter(featureExtractionParams.tileWidth == featXtractParams.tileWidth)
+            .filter(
+                featureExtractionParams.magnification == featXtractParams.magnification
+            )
+            .first()
+        )
+        if exist:
+            return exist
+        else:
+            ftr = session.add(featXtractParams)
+            session.commit()
+            session.refresh(featXtractParams)
+            return featXtractParams  ## This should return a fresh copy of the extracted feature..
+
+    return None
+
+
+@app.post("/lookupFeatureExtractionParams")
+async def lookup_featureExtractionParams(
+    imageId: str, magnification: float, tileSizeParam: str
+):
+    print(
+        "This will determine if a set of feature extractions have already been run at this resolution"
+    )
+    with Session(engine) as session:
+        exist = (
+            session.query(featureExtractionParams)
+            .filter(featureExtractionParams.imageId == imageId)
+            .filter(featureExtractionParams.tileSizeParam == tileSizeParam)
+            .filter(featureExtractionParams.magnification == magnification)
+            .first()
+        )
+        if exist:
+            return exist
+        else:
+            return None
     return None
 
 
 @app.get("/lookupImage")
 async def lookupImage(imageName: str, dsaApiUrl: str):
     with Session(engine) as session:
-        imageInfo = session.query(DSAImage).filter(DSAImage.imageName == imageName).first()
+        imageInfo = (
+            session.query(DSAImage).filter(DSAImage.imageName == imageName).first()
+        )
         # return imageInfo
         return imageInfo
 
@@ -285,3 +351,31 @@ async def get_imageList():
 #         session.commit()
 #         session.refresh(city)
 #         return city
+
+# @app.post("/add-station/")
+# async def add_station(station: Stations):
+#     with Session(engine) as session:
+#         exist = (
+#             session.query(Stations)
+#             .filter(Stations.station_id == station.station_id)
+#             .first()
+#         )
+#         if exist:
+#             raise HTTPException(status_code=400, detail="Station already exists")
+
+#         point = f"POINT({station.longitude} {station.latitude})"
+#         station.geom = WKTElement(point, srid=4326)
+
+#         session.add(station)
+#         session.commit()
+#         session.refresh(station)
+
+#         to_return = {}
+#         to_return["station_id"] = station.station_id
+#         to_return["latitude"] = station.latitude
+#         to_return["longitude"] = station.longitude
+#         to_return["cp"] = station.cp
+#         to_return["city"] = station.city
+#         to_return["address"] = station.adress
+
+#         return to_return
--- fastapi/app/models.py
+++ fastapi/app/models.py
@@ -1,7 +1,7 @@
 from typing import Optional
 from sqlmodel import Field, SQLModel, Integer, Float
 from datetime import datetime
-from sqlalchemy import Column
+from sqlalchemy import Column, BigInteger
 from geoalchemy2.types import Geometry
 from typing import Any, List, Optional
 
@@ -21,78 +21,38 @@ class DSAImage(SQLModel, table=True):
     tileHeight: int
 
 
-from sqlalchemy.dialects import postgresql  # ARRAY contains requires dialect specific type
-
-#     tags: Optional[Set[str]] = Field(default=None, sa_column=Column(postgresql.ARRAY(String())))
-
-# (...)
-#         tagged = session.query(Item).filter(Item.tags.contains([tag]))
-
-from typing import List
-
-# from sqlmodel import Field, Session, SQLModel, create_engine, JSON, Column
-
-
-# class Block(SQLModel, table=True):
-#     id: int = Field(..., primary_key=True)
-#     values: List[str] = Field(sa_column=Column(JSON))
-
-#     # Needed for Column(JSON)
-#     class Config:
-#         arbitrary_types_allowed = True
-
-
-# engine = create_engine("sqlite:///test_database.db", echo=True)
-
-# SQLModel.metadata.create_all(engine)
-
-# b = Block(id=0, values=['test', 'test2'])
-# with Session(engine) as session:
-#     session.add(b)
-#     session.commit()
+from sqlalchemy.dialects import (
+    postgresql,
+)  # ARRAY contains requires dialect specific type
 
 
 class tileFeatures(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     apiURL: str
-    imageId: str
+    imageId: str = Field(index=True)
     imageName: str
     topX: int
     topY: int
-    red: Optional[List[int]] = Field(default=None, sa_column=Column(postgresql.ARRAY(Integer())))
-    green: Optional[List[int]] = Field(default=None, sa_column=Column(postgresql.ARRAY(Integer())))
-    blue: Optional[List[int]] = Field(default=None, sa_column=Column(postgresql.ARRAY(Integer())))
-    average: Optional[List[float]] = Field(default=None, sa_column=Column(postgresql.ARRAY(Float())))
+    width: int
+    height: int
+    featureType: str  ### Should be something like imgHistogram
+    ftxtract_id: int
+    red: Optional[List[int]] = Field(
+        default=None, sa_column=Column(postgresql.ARRAY(Integer()))
+    )
+    green: Optional[List[int]] = Field(
+        default=None, sa_column=Column(postgresql.ARRAY(Integer()))
+    )
+    blue: Optional[List[int]] = Field(
+        default=None, sa_column=Column(postgresql.ARRAY(Integer()))
+    )
+    average: Optional[List[float]] = Field(
+        default=None, sa_column=Column(postgresql.ARRAY(Float()))
+    )
+
     # Needed for Column(JSON)
     class Config:
         arbitrary_types_allowed = True
-
-
-class Cities(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    postal_code: str
-    name: str
-    lat: float
-    lon: float
-
-
-class GasPrices(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    station_id: str
-    oil_id: str
-    nom: str
-    valeur: float
-    maj: datetime = Field(default_factory=datetime.utcnow)
-
-
-class Stations(SQLModel, table=True):
-    station_id: str = Field(primary_key=True)
-    latitude: float
-    longitude: float
-    cp: str
-    city: str
-    adress: str
-    geom: Optional[Any] = Field(sa_column=Column(Geometry("GEOMETRY")))
 
 
 class SimpleRectangles(SQLModel, table=True):
@@ -117,3 +77,81 @@ class MoreSimpleRectangles(SQLModel, table=True):
     shapeName: str
     shapeLabel: str
     shapeLocation: Optional[Any] = Field(sa_column=Column(Geometry("GEOMETRY")))
+
+
+class featureExtractionParams(SQLModel, table=True):
+    ftxtract_id: int = Field(primary_key=True, default=None)
+    tileWidth: int
+    tileHeight: int
+    featureType: str
+    ftxComputeTime: float
+    bytesRead: Optional[int] = Field(sa_column=Column(BigInteger()), default=None)
+    imageId: str
+    tilesProcessed: int
+    sizeX: int
+    sizeY: int
+    magnification: float
+    tileSizeParam: Optional[str] = Field(default=None)
+    ## This helps me keep track of if I used a native tile sizing or multiple threreof
+
+    # {'imageName': 'TCGA-19-1787-01C-01-TS1.b9f6f0f2-14f2-4bc5-b7f8-9520ec38eb98.svs',
+
+
+# 'imagePath': 'SmallSampleFiles/TCGA-19-1787-01C-01-TS1.b9f6f0f2-14f2-4bc5-b7f8-9520ec38eb98.svs',
+# 'opType': 'tileHist', 'startTime': 1683232143.5455363,
+# 'elapsedTime': 0.13014698028564453, 'tilesProcessed': 4,
+# 'tileWidth': 256, 'tileHeight': 256, 'sizeX': 6000,
+#  'sizeY': 7977, 'bytesRead': 747000, 'magnification': 1.25,
+# 'tileSizeParam': 'native'}
+
+#     tags: Optional[Set[str]] = Field(default=None, sa_column=Column(postgresql.ARRAY(String())))
+
+# (...)
+#         tagged = session.query(Item).filter(Item.tags.contains([tag]))
+
+# from sqlmodel import Field, Session, SQLModel, create_engine, JSON, Column
+
+
+# class Block(SQLModel, table=True):
+#     id: int = Field(..., primary_key=True)
+#     values: List[str] = Field(sa_column=Column(JSON))
+
+#     # Needed for Column(JSON)
+#     class Config:
+#         arbitrary_types_allowed = True
+
+
+# engine = create_engine("sqlite:///test_database.db", echo=True)
+
+# SQLModel.metadata.create_all(engine)
+
+# b = Block(id=0, values=['test', 'test2'])
+# with Session(engine) as session:
+#     session.add(b)
+#     session.commit()
+
+# class Stations(SQLModel, table=True):
+#     station_id: str = Field(primary_key=True)
+#     latitude: float
+#     longitude: float
+#     cp: str
+#     city: str
+#     adress: str
+#     geom: Optional[Any] = Field(sa_column=Column(Geometry("GEOMETRY")))
+
+
+# class Cities(SQLModel, table=True):
+#     id: Optional[int] = Field(default=None, primary_key=True)
+#     postal_code: str
+#     name: str
+#     lat: float
+#     lon: float
+
+
+# class GasPrices(SQLModel, table=True):
+#     id: Optional[int] = Field(default=None, primary_key=True)
+#     station_id: str
+#     oil_id: str
+#     nom: str
+#     valeur: float
+#     maj: datetime = Field(default_factory=datetime.utcnow)
