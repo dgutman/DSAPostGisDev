d242954fa657d1a216c9653855e183515d0163f4
David Gutman <dagutman@gmail.com>
2023-10-20 15:49:21 -0400

    merging annotation Panel changes

--- annotationBrowser/annotationBrowser.py
+++ annotationBrowser/annotationBrowser.py
@@ -29,10 +29,30 @@ from annotationTableView import annotationTable_layout
 from app_config import app
 
 # Dash app setup
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.layout = html.Div(tab_layout)
+## APP INSTANTIATION OCCURS ON THE APP_CONFIG FILE AS A SINGLETON
+
+app.layout = html.Div(
+    [
+        dmc.Tabs(
+            [
+                dmc.TabsList(
+                    [
+                        dmc.Tab("Annotation DataView", value="annotationDataview"),
+                        dmc.Tab("Annotation Table", value="annotationTable"),
+                    ]
+                ),
+                dmc.TabsPanel(annotation_panel, value="annotationDataview"),
+                dmc.TabsPanel(annotationTable_layout, value="annotationTable"),
+            ],
+            variant="pills",
+            color="#6384c6",
+            value="annotationTable",
+        ),
+    ],
+)
 
 ## WILL FOR NOW EMBED ALL THE GIRDER_CLIENT LOGIN AND LOGIC IN HERE... SINCE THIS WILL BE SEPARATE FROM THE PANEL ITSELF IN OUR FINAL APP
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server(debug=True,host='0.0.0.0')
