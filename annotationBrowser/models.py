41e734488bdd8ce3f62b9043918dcde177e47ef9
arjita2803 <arjita2803@gmail.com>
2023-11-06 16:44:38 -0500

    added mask contours to tissueSegModel

---
+++ annotationBrowser/models.py
@@ -0,0 +1,181 @@
+import cv2 as cv
+from pandas import DataFrame, read_csv
+import numpy as np
+import json
+from pathlib import Path
+import torch
+import torch.nn as nn
+import torchvision.models as models
+from os.path import isfile
+import matplotlib.pyplot as plt
+
+from PIL import Image
+from typing import Union, List, Tuple, Optional
+import random
+from torch import Tensor
+
+import torchvision.transforms as transforms
+import torchvision.transforms.functional as F
+from torchvision.transforms import InterpolationMode
+
+
+class Compose(object):
+    """Apply a list of semantic segmentation transforms to an image and label
+    mask.
+
+    Args:
+        transforms: Transforms to apply to image and mask in order.
+
+    Attributes:
+        transforms (list): List of transforms.
+    
+    """
+    def __init__(self, transforms: list):
+        self.transforms = transforms
+        
+    def __call__(self, image: Image):
+        """
+        Args:
+            image: Image.
+            mask: Label mask.
+
+        Returns:
+            Image and mask after transformations.
+        
+        """
+        for transform in self.transforms:
+            image = transform(image)
+            
+        return image
+
+
+class ToTensor(object):
+    """Transform image and mask to tensor.     
+    
+    """
+    def __call__(self, image):
+        """    
+        Args:
+            image: Image.
+            mask: Label mask.
+
+        Returns:
+            Image and mask as tensors.
+        
+        """
+        # convert image to tensor
+        transform = transforms.ToTensor()
+        image = transform(image)
+        
+        return image
+
+
+class Normalize(object):
+    """Color normalize an image, label mask is passed but not normalized.
+    
+    Args:
+        mean: RGB mean.
+        std: RGB standard deviation.
+        
+    Attributes:
+        transform (torchvision.transforms.Normalize): Normailze transform.
+        
+    """
+    def __init__(self, mean: [int, int, int], std: [int, int, int]):
+        self.transform = transforms.Normalize(mean=mean, std=std)
+        
+    def __call__(self, image):
+        """
+        Args:
+            image: Image.
+            mask: Label mask.
+
+        Returns:
+            Image and mask as tensors.
+        
+        """
+        return self.transform(image)
+
+
+class Resize(object):
+    """Resize input image or tensor and its label mask. Label mask is always
+    resized with nearest exact interpolation to keep only unique labels.
+    
+    Args:
+        size: Size to resize input to.
+        interpolation: Interpolation method for image, see docstring for 
+            torchvision.transforms.Resize for details.
+        antialias: Apply antialias to the image, see docstring for 
+            torchvision.transforms.Resize for details.
+
+    Attributes:
+        transform (torchvision.transforms.Resize): Transform applied to image.
+        label_transform (torchvision.transforms.Resize): Transform applied to
+            label masks.
+            
+    """
+    def __init__(
+        self, 
+        size: int , 
+        interpolation: InterpolationMode = InterpolationMode.BILINEAR, 
+        antialias: bool = True
+    ):
+        self.transform = transforms.Resize(size, interpolation=interpolation,
+                                           antialias=antialias)
+        self.label_transform = transforms.Resize(
+            size, interpolation=InterpolationMode.NEAREST_EXACT
+        )
+
+    def __call__(self, image):
+        """
+        Args:
+            image: Image.
+            mask: Label mask.
+
+        Returns:
+            Image and mask as tensors.
+
+        """
+        return self.transform(image)
+
+
+size = 512
+
+norm_mean = [0.485, 0.456, 0.406]
+norm_std = [0.229, 0.224, 0.225]
+
+val_transforms = Compose([
+    ToTensor(),
+    Resize(size),
+    Normalize(mean=norm_mean, std=norm_std)
+])
+
+class uNetResNet(nn.Module):
+    def __init__(self, in_channels, out_channels, pretrained=True):
+        super(uNetResNet, self).__init__()   
+        # Load the ResNet-34 model
+        resnet = models.resnet34(pretrained=pretrained)        
+        self.encoder = nn.Sequential(
+            resnet.conv1,
+            resnet.bn1,
+            resnet.relu,
+            resnet.layer1,
+            resnet.layer2,
+            resnet.layer3,
+            resnet.layer4
+        )
+        self.decoder = nn.Sequential(
+            nn.Conv2d(512, 256, kernel_size=1),  
+            nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1),  
+            nn.ReLU(),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),  
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.ConvTranspose2d(64, out_channels, kernel_size=4, stride=2, padding=1)  
+        )
+    
+    def forward(self, x):
+        x1 = self.encoder(x)
+        x2 = self.decoder(x1)
+        return x2
--- annotationBrowser/tissueSegModel_panel.py
+++ annotationBrowser/tissueSegModel_panel.py
@@ -1,10 +1,17 @@
+import sys
 from dash import html, Input, Output, State, callback
 from settings import gc
 from dbHelpers import generate_generic_DataTable
 import pandas as pd
 import dash_bootstrap_components as dbc
 from dataView_component import getImageThumb_as_NP, plotImageAnnotations
-
+from models import uNetResNet
+import torch
+import cv2 as cv
+import dash_core_components as dcc
+import plotly.graph_objs as go
+import numpy as np
+from models import val_transforms
 sampleFolderId = "645b9e006df8ba8751a909dd"
 
 
@@ -23,6 +30,31 @@ tissueSegModel_panel = html.Div(
     ]
 )
 
+model = uNetResNet(in_channels=3, out_channels=1,pretrained=True) 
+model.load_state_dict(
+      torch.load('best.pt', 
+                  map_location=torch.device('cpu'))
+   )
+model.eval()
+
+def reshape_with_pad(img, size, pad = (255, 255, 255)):
+    """Reshape an image into a square aspect ratio without changing the original
+    image aspect ratio - i.e. use padding.
+    
+    """
+    h, w = img.shape[:2]
+
+    if w > h:
+        img = cv.copyMakeBorder(img, 0, w-h, 0, 0, cv.BORDER_CONSTANT, None, 
+                                pad)
+    else:
+        img = cv.copyMakeBorder(img, 0, 0, 0, h-w, cv.BORDER_CONSTANT, None, 
+                                pad)
+
+    # Reshape the image.
+    img = cv.resize(img, (size, size), None, None, cv.INTER_NEAREST)
+
+    return img
 
 @callback(
     Output("modelOutput_div", "children"),
@@ -35,7 +67,49 @@ def selected(selected):
         #        image_as_np = getImageThumb_as_NP(selected[0]["_id"])
 
         image_fig = plotImageAnnotations(imageId)
+        image_copy= getImageThumb_as_NP(imageId)
+        image_with_contours = image_copy.copy()
+        orig_shape = (image_copy.shape[1], image_copy.shape[0])
+        image_copy = image_copy[:, :, :3]
+        image = val_transforms(image_copy)
+        with torch.set_grad_enabled(False):
+            image = image.unsqueeze(0)
+            pred = model(image)
+        pred = (pred[0][0] > 0.5).data.cpu().numpy()
+        pred = pred.astype(np.uint8) * 255
+        pred = cv.resize(pred, (orig_shape[0], orig_shape[1]), interpolation=cv.INTER_NEAREST)
+        contours, hierarchy  = cv.findContours(pred, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        epsilon = 0.002 * cv.arcLength(contours[0], True)  
+        #smoothed_contours = [cv.approxPolyDP(cnt, epsilon, True) for cnt in contours]
+        #image_trace = go.Image(z=image_copy)
+        #mask_trace = go.Contour(z=pred, showscale=False, contours=dict(showlines=False))
+        cv.drawContours(image_with_contours, contours, -1, (0, 0, 255), 2)
+        image_with_contours_trace = go.Image(z=image_with_contours)
+        # smoothed_contours_trace = go.Scatter(
+        #     x=[point[0] for point in smoothed_contours[0][:, 0, 0]],
+        #     y=[point[0] for point in smoothed_contours[0][:, 0, 1]],
+        #     mode='lines',
+        #     line=dict(color='red', width=2)
+        # )
 
-        return html.Div([html.Div(selected[0]["name"]), image_fig])
+        layout = go.Layout(
+            title=selected[0]["name"],
+            showlegend=False
+        )
+
+        #fig = go.Figure(data=[image_trace, mask_trace], layout=layout)
+        fig = go.Figure(data=[image_with_contours_trace], layout=layout)
+
+        return dbc.Container([
+            dbc.Row([
+                dbc.Col(html.Div(selected[0]["name"]))
+            ]),
+            dbc.Row([
+                dbc.Col(image_fig), 
+                dbc.Col(dcc.Graph(figure=fig))
+            ])
+        ])
 
     return "No selections"
+
+
