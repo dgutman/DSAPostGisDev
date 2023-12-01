a6b769b5593d6fa418173a5bb24cd310fde043ef
David Gutman <dagutman@gmail.com>
2023-12-01 11:43:49 -0500

    adding path definitions at the top of yolo

--- annotationBrowser/yolo.py
+++ annotationBrowser/yolo.py
@@ -6,7 +6,18 @@ import numpy as np
 from tqdm import tqdm
 from PIL import Image
 from io import BytesIO
-from utils import get_tile_metadata, imwrite, get_filename, blob_detect, read_yolo_label, im_to_txt_path,corners_to_polygon,convert_box_type,tile_roi_with_labels, tile_roi_with_labels_wrapper
+from utils import (
+    get_tile_metadata,
+    imwrite,
+    get_filename,
+    blob_detect,
+    read_yolo_label,
+    im_to_txt_path,
+    corners_to_polygon,
+    convert_box_type,
+    tile_roi_with_labels,
+    tile_roi_with_labels_wrapper,
+)
 from os import makedirs
 from os.path import join
 from tqdm import tqdm
@@ -28,9 +39,9 @@ DSA_BASE_URL = "http://glasslab.neurology.emory.edu:8080/api/v1"
 gc = girder_client.GirderClient(apiUrl=DSA_BASE_URL)
 load_dotenv(dotenv_path=".env", override=True)
 YoloKEY = os.environ.get("YoloKEY", None)
-print('This is the value of the YOLO key', YoloKEY)
+print("This is the value of the YOLO key", YoloKEY)
 if YoloKEY:
-    print('This is the API key:', YoloKEY)
+    print("This is the API key:", YoloKEY)
     gc.authenticate(apiKey=YoloKEY)
 
 run_yolo = html.Div(
@@ -57,12 +68,15 @@ run_yolo = html.Div(
                     ],
                     width=2,
                 ),
-                dbc.Col([dbc.Button(
+                dbc.Col(
+                    [
+                        dbc.Button(
                             "Get Yolo Model Predictions",
                             id="get-yolo-predictions-button",
                             className="me-2",
-                        )], 
-                        width=2
+                        )
+                    ],
+                    width=2,
                 ),
             ]
         ),
@@ -80,8 +94,25 @@ run_yolo = html.Div(
 
 
 ##saving the images locally right now, will need to change this
-save_dir = '/Users/arjita/Desktop/GrayMatter/DSAPostGisDev/annotationBrowser/yolo/images'
-src_dir_labels = '/Users/arjita/Desktop/GrayMatter/DSAPostGisDev/annotationBrowser/yolo'
+save_dir = "yolo/images"
+src_dir_labels = "yolo"
+YOLO_OPTIMUS_MODEL = "yolo/models/best.pt"
+YOLO_INPUT_TILE_DIR = "yolo/tiles/images/"
+IMAGE_SAVE_DIR = "yolo"
+predictions_folder = "runs/detect/predict"
+
+
+# Check or create directories
+for p in [
+    YOLO_INPUT_TILE_DIR,
+    save_dir,
+    src_dir_labels,
+    IMAGE_SAVE_DIR,
+    predictions_folder,
+]:
+    if not os.path.isdir(p):
+        os.makedirs(p, exist_ok=True)
+
 
 def parse_args():
     """CLI arguments."""
@@ -94,7 +125,6 @@ def parse_args():
         api_url="http://glasslab.neurology.emory.edu:8080/api/v1",
     )
     return args
-
 
 
 def get_images(fld_id):
@@ -136,24 +166,21 @@ def get_images(fld_id):
         img = np.array(Image.open(BytesIO(response.content)))
         imwrite(join(save_dir, f"{get_filename(item['name'])}.png"), img)
 
-        #Get labels
-    img_fps = sorted([fp for fp in glob.glob(join(save_dir, '*.png'))])
+        # Get labels
+    img_fps = sorted([fp for fp in glob.glob(join(save_dir, "*.png"))])
     if not img_fps:
         print("No PNG files found in the specified directory.")
     else:
-        label_dir = join(src_dir_labels, 'labels')
+        label_dir = join(src_dir_labels, "labels")
         makedirs(label_dir, exist_ok=True)
         print(f"Found {len(img_fps)} PNG files.")
-    label_dir = join(src_dir_labels, 'labels')
+    label_dir = join(src_dir_labels, "labels")
     makedirs(label_dir, exist_ok=True)
-    kwargs = {
-    'max_sigma': 30, 
-    'num_sigma': 15, 
-    'threshold': 0.05
-    }
+    kwargs = {"max_sigma": 30, "num_sigma": 15, "threshold": 0.05}
 
     for fp in tqdm(img_fps):
         _ = blob_detect(fp, kwargs=kwargs, save_dir=label_dir, plot=False)
+
 
 @callback(
     Output("get-images-button", "children"),
@@ -167,10 +194,12 @@ def update_images(n_clicks):
     get_images(args.fld_id)
     return "Images and Blobs retrieved and saved."
 
-SAVE_DIR = '/Users/arjita/Desktop/GrayMatter/DSAPostGisDev/annotationBrowser/yolo'
-fps = glob.glob("/Users/arjita/Desktop/GrayMatter/DSAPostGisDev/annotationBrowser/yolo/images/*.png")
+
+fps = glob.glob(IMAGE_SAVE_DIR + "*.png")
+
+
 def get_tiles_and_yolo_dataset():
-    tiles_dir = join(SAVE_DIR, 'tiles')
+    tiles_dir = join(save_dir, "tiles")
     tiles_df = tile_roi_with_labels_wrapper(
         fps,
         tiles_dir,
@@ -178,28 +207,32 @@ def get_tiles_and_yolo_dataset():
         stride=960,
         fill=0,
         notebook=True,
-        grayscale=False
+        grayscale=False,
     )
     updated_fps = sorted(fps)
     train_fps, val_fps = train_test_split(updated_fps, train_size=0.8)
-    val_txt_fp = join(SAVE_DIR, 'val.txt')
+    val_txt_fp = join(IMAGE_SAVE_DIR, "val.txt")
 
-    with open(join(SAVE_DIR, 'dataset.yaml'), 'w') as fh:
+    with open(join(IMAGE_SAVE_DIR, "dataset.yaml"), "w") as fh:
         yaml.safe_dump(
-            {'nc': 1, 'names': ['nuclei'], 'path': SAVE_DIR, 'train': 'train.txt',
-            'val': 'val.txt'}, 
-            fh
+            {
+                "nc": 1,
+                "names": ["nuclei"],
+                "path": IMAGE_SAVE_DIR,
+                "train": "train.txt",
+                "val": "val.txt",
+            },
+            fh,
         )
-        
-    with open(join(SAVE_DIR, 'train.txt'), 'w') as fh:
+
+    with open(join(IMAGE_SAVE_DIR, "train.txt"), "w") as fh:
         fh.write(
-            '\n'.join(tiles_df[tiles_df.roi_fp.isin(train_fps)].fp.tolist()).strip()
+            "\n".join(tiles_df[tiles_df.roi_fp.isin(train_fps)].fp.tolist()).strip()
         )
-        
-    with open(join(SAVE_DIR, 'val.txt'), 'w') as fh:
-        fh.write(
-            '\n'.join(tiles_df[tiles_df.roi_fp.isin(val_fps)].fp.tolist()).strip()
-        )
+
+    with open(join(IMAGE_SAVE_DIR, "val.txt"), "w") as fh:
+        fh.write("\n".join(tiles_df[tiles_df.roi_fp.isin(val_fps)].fp.tolist()).strip())
+
 
 @callback(
     Output("get-tiles-button", "children"),
@@ -212,12 +245,20 @@ def update_tiles(n_clicks):
     get_tiles_and_yolo_dataset()
     return "Tiles and Yolo dataset retrieved and saved."
 
+
+## This is the model trained on optimus
 def run_yolo_model():
-    model = YOLO('/Users/arjita/Desktop/GrayMatter/DSAPostGisDev/annotationBrowser/yolo/best.pt')
+    model = YOLO(YOLO_OPTIMUS_MODEL)
     results = model.predict(
-        '/Users/arjita/Desktop/GrayMatter/DSAPostGisDev/annotationBrowser/yolo/tiles/images/',
-        conf=0.3, save_txt=True, save_conf=True, save=True, hide_conf=True, hide_labels=True
+        YOLO_INPUT_TILE_DIR,
+        conf=0.3,
+        save_txt=True,
+        save_conf=True,
+        save=True,
+        hide_conf=True,
+        hide_labels=True,
     )
+
 
 @callback(
     Output("get-yolo-predictions-button", "children"),
@@ -230,7 +271,6 @@ def update_predictions(n_clicks):
     run_yolo_model()
     return "Predictions retrieved and saved."
 
-predictions_folder = "/Users/arjita/Desktop/GrayMatter/DSAPostGisDev/annotationBrowser/runs/detect/predict"
 
 def read_images(folder):
     images = []
@@ -239,23 +279,34 @@ def read_images(folder):
             path = os.path.join(folder, filename)
             with open(path, "rb") as f:
                 encoded_image = base64.b64encode(f.read()).decode("utf-8")
-                images.append({"name": filename, "image": f"data:image/png;base64,{encoded_image}"})
+                images.append(
+                    {
+                        "name": filename,
+                        "image": f"data:image/png;base64,{encoded_image}",
+                    }
+                )
     return images
 
-@callback(
-    Output("image-display", "children"),
-    [Input("image-display", "id")]
-)
+
+@callback(Output("image-display", "children"), [Input("image-display", "id")])
 def update_images(_):
     images = read_images(predictions_folder)
-    
+
     # Generate cards for each image
     image_cards = [
-        dbc.Col(dbc.Card(
-            dbc.CardImg(id=f"image-{i}", src=image["image"], top=True, style={"width": "100%", "height": "auto"})
-        )) for i, image in enumerate(images)
+        dbc.Col(
+            dbc.Card(
+                dbc.CardImg(
+                    id=f"image-{i}",
+                    src=image["image"],
+                    top=True,
+                    style={"width": "100%", "height": "auto"},
+                )
+            )
+        )
+        for i, image in enumerate(images)
     ]
-    
-    rows = [dbc.Row(image_cards[i:i+6]) for i in range(0, len(image_cards), 6)]
-    
+
+    rows = [dbc.Row(image_cards[i : i + 6]) for i in range(0, len(image_cards), 6)]
+
     return rows
